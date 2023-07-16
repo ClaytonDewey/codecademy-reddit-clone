3202aca27b5382e169bf4d0cd2e3351b91dc60f4
Clayton Dewey <clay@dryadmedia.com>
2023-07-15 22:21:41 -0500

    Reformat header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,11 @@ import Nav from './Nav';
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <h1 className='mx-auto text-center'>Reddit Clone</h1>
+    </>
+  );
 };
 export default Header;
