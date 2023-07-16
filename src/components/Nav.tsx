50568510dcc64306eb90023f519822cdb350a776
Clayton Dewey <clay@dryadmedia.com>
2023-07-15 22:14:05 -0500

    Add images folder

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  display: flex;
+  background-color: var(--white);
+  height: 4.4rem;
+  padding: 6px 12px;
+
+  ul {
+    list-style: none;
+    display: flex;
+
+    a {
+      padding: 0 1rem;
+    }
+  }
+
+  border-top: 1px solid var(--black);
+  border-bottom: 1px solid var(--black);
+`;
 
 type NavProps = {};
 
 const Nav: React.FC<NavProps> = () => {
   return (
-    <nav>
+    <StyledNav>
       <ul>
         <li>
           <a href='/'>somewhere</a>
@@ -13,7 +32,7 @@ const Nav: React.FC<NavProps> = () => {
           <a href='/'>somewhere else</a>
         </li>
       </ul>
-    </nav>
+    </StyledNav>
   );
 };
 export default Nav;
