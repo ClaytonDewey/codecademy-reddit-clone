3f7a86855cee4387e3c69f9fccaf97ea5fe70a85
Clayton Dewey <clay@dryadmedia.com>
2023-07-15 22:14:21 -0500

    Move h1 to Header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,6 @@ const App: React.FC<AppProps> = () => {
   return (
     <div className='App'>
       <GlobalStyle />
-      <h1 className='mx-auto my-4 text-center'>Reddit Clone</h1>
       <Header />
       <Posts />
       <Sidebar />
