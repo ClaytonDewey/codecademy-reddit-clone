4f1f876c19b0ac0940d3aa70ea1f4a621a6dbce0
Clayton Dewey <clay@dryadmedia.com>
2023-07-15 22:04:48 -0500

    Reformat App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,20 @@
 import GlobalStyle from '../theme/global';
+import '../theme/main.scss';
 import Header from './Header';
 import Posts from './Posts';
 import Sidebar from './Sidebar';
 
-function App() {
+type AppProps = {};
+
+const App: React.FC<AppProps> = () => {
   return (
     <div className='App'>
       <GlobalStyle />
-      <h1>Reddit Clone</h1>
+      <h1 className='mx-auto my-4 text-center'>Reddit Clone</h1>
       <Header />
       <Posts />
       <Sidebar />
     </div>
   );
-}
-
+};
 export default App;
