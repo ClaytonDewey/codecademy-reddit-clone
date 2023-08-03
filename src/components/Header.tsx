7c4ede719c3070b5a61bbf46f0781c27183b39b7
ClaytonDewey <clay@dryadmedia.com>
2023-08-03 10:53:29 -0500

    Reformat header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
-import Nav from './Nav';
+import styled from 'styled-components';
+import redditFace from '../images/redditFace.svg';
+import redditText from '../images/redditText.svg';
+import { FaSearch } from 'react-icons/fa';
 
+const StyledHeader = styled.header`
+  grid-column: 1/-1;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: var(--spacing-2);
+  align-content: center;
+  color: var(--color-branding);
+  background: var(--color-foreground);
+  box-shadow: var(--box-shadow);
+  height: 64px;
+  padding: 0 var(--spacing-5);
+
+  .logo {
+    display: flex;
+    align-items: center;
+    width: 4rem;
+    margin-right: 2rem;
+  }
+
+  .search {
+    display: flex;
+    align-items: center;
+  }
+
+  .search input {
+    flex-grow: 1;
+    background: var(--color-background-secondary);
+    border: none;
+    padding: var(--spacing-1) var(--spacing-2);
+    height: max-content;
+    border-radius: var(--radius);
+  }
+
+  .search button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    color: var(--color-text-secondary);
+    font-size: 1.5rem;
+  }
+`;
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
   return (
-    <>
-      <Nav />
-      <h1 className='mx-auto text-center'>Reddit Clone</h1>
-    </>
+    <StyledHeader>
+      <div className='logo'>
+        <img src={redditFace} height='30px' alt='logo' role='presentation' />
+        <img src={redditText} height='46px' alt='reddit' role='presentation' />
+      </div>
+      <form className='search'>
+        <input type='text' placeholder='Search' aria-label='Search Posts' />
+        <button type='submit' aria-label='Search'>
+          <FaSearch />
+        </button>
+      </form>
+    </StyledHeader>
   );
 };
 export default Header;
