a12cd64361f834293813714fd28c2aec26f1e82e
ClaytonDewey <clay@dryadmedia.com>
2023-08-03 13:14:55 -0500

    Setup base styles for Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type SidebarProps = {};
 
+const StyledSidebar = styled.div`
+  display: none;
+  flex: 0 0 312px;
+  width: 312px;
+  margin-left: var(--spacing-3);
+
+  @media screen and (min-width: 960px) {
+    display: block;
+  }
+`;
+
 const Sidebar: React.FC<SidebarProps> = () => {
-  return <div>I am the sidebar</div>;
+  return <StyledSidebar>I am the sidebar</StyledSidebar>;
 };
 export default Sidebar;
