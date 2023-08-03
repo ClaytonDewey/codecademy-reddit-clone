184555abbb0f106d50638c08374fa2ca2b249bca
ClaytonDewey <clay@dryadmedia.com>
2023-08-03 17:39:56 -0500

    Start styling sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,12 +9,29 @@ const StyledSidebar = styled.div`
   width: 312px;
   margin-left: var(--spacing-3);
 
+  .card {
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    margin-bottom: 10px;
+    overflow: hidden;
+    position: relative;
+    cursor: pointer;
+    padding: 8px;
+  }
+
   @media screen and (min-width: 960px) {
     display: block;
   }
 `;
 
 const Sidebar: React.FC<SidebarProps> = () => {
-  return <StyledSidebar>I am the sidebar</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <div className='card'>
+        <h2>Subreddits</h2>
+      </div>
+    </StyledSidebar>
+  );
 };
 export default Sidebar;
