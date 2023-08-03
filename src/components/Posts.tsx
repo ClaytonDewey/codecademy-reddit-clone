feac709e4e9a018acbe19bc1df002d76d79440d8
ClaytonDewey <clay@dryadmedia.com>
2023-08-03 13:15:30 -0500

    Setup base styles for Posts wrapper

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Post from './Post';
+import { styled } from 'styled-components';
 
 type PostsProps = {};
 
+const StyledPostsWrapper = styled.div`
+  min-width: 0;
+  width: 100%;
+
+  @media screen and (min-width: 640px) {
+    width: 640px;
+  }
+`;
+
 const Posts: React.FC<PostsProps> = () => {
   return (
-    <div>
+    <StyledPostsWrapper>
       <h2>Posts</h2>
       <Post />
       <Post />
       <Post />
       <Post />
-    </div>
+    </StyledPostsWrapper>
   );
 };
 export default Posts;
