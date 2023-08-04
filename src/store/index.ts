2bd55cdd989d5093f02fa64acaa167ff8558dc66
ClaytonDewey <clay@dryadmedia.com>
2023-08-04 15:51:08 -0500

    Update exports & add types

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,14 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import redditReducer from './redditSlice';
 import subRedditReducer from './subRedditslice';
 
-export default configureStore({
+const store = configureStore({
   reducer: combineReducers({
     reddit: redditReducer,
     subreddits: subRedditReducer,
   }),
 });
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
