3a3ff6011cd2ba1f09465732f8f086cc6b089c6c
Clayton Dewey <clay@dryadmedia.com>
2023-08-05 12:04:42 -0500

    Update default subreddit

--- src/store/redditSlice.ts
+++ src/store/redditSlice.ts
@@ -8,7 +8,7 @@ const initialState = {
   error: false,
   isLoading: false,
   searchTerm: '',
-  selectedSubreddit: '/r/pics/',
+  selectedSubreddit: '/r/reddeadredemption/',
 };
 
 const redditSlice = createSlice({
