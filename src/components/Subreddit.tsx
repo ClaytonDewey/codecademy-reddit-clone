a012a4867937e581055b63f3914ed6284aa29307
Clayton Dewey <clay@dryadmedia.com>
2023-08-06 12:30:09 -0500

    Revert "Remove unneeded imports"
    This reverts commit a386379a399523cdb8f160089d3768f9540e5369.

--- src/components/Subreddit.tsx
+++ src/components/Subreddit.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import icon from '../assets/subRedditIcon.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../store';
+import { useAppSelector } from '../hooks';
 import { fetchSubreddits, selectSubreddits } from '../store/subRedditslice';
 import {
   setSelectedSubreddit,
