ed820ea62f85f32e228ec586415db177c4dbc470
Clayton Dewey <clay@dryadmedia.com>
2023-08-06 21:47:03 -0500

    Change ul/li to dropdown

--- src/components/Subreddit.tsx
+++ src/components/Subreddit.tsx
@@ -1,90 +1,44 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import icon from '../assets/subRedditIcon.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../store';
-import { useAppSelector } from '../hooks';
 import { fetchSubreddits, selectSubreddits } from '../store/subRedditslice';
 import {
   setSelectedSubreddit,
   selectSelectedSubreddit,
 } from '../store/redditSlice';
 
-type SubredditProps = {};
-
-const StyledContainer = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
+const StyledDropdown = styled.select`
+  /* border: none; */
+  background: var(--color-branding-transparent);
+  width: 100%;
+  padding: 8px;
+  border-radius: 4px;
+  cursor: pointer;
 `;
 
-const StyledItem = styled.li`
-  button {
-    display: flex;
-    align-items: center;
-    font-weight: 700;
-    font-size: 12px;
-    color: var(--color-text-body);
-    padding: var(--spacing-1);
-    border: none;
-    background: transparent;
-    cursor: pointer;
-    transition: opacity 0.25s ease-in-out;
-    width: 100%;
+type DropDownProps = {};
 
-    &:hover {
-      opacity: 0.75;
-    }
-  }
-
-  .icon {
-    height: 25px;
-    width: 25px;
-    border-radius: 50%;
-    margin-right: var(--spacing-1);
-    border: 3px solid;
-    box-sizing: content-box;
-  }
-
-  &.active {
-    background-color: var(--color-branding-transparent);
-    border-left: 3px solid;
-  }
-`;
-
-const Subreddit: React.FC<SubredditProps> = () => {
+const DropDown: React.FC<DropDownProps> = () => {
   const dispatch: AppDispatch = useDispatch();
   const subreddits = useSelector(selectSubreddits);
   const selectedSubreddit = useSelector(selectSelectedSubreddit);
+
+  const handleChange = (e) => {
+    console.log(selectedSubreddit, e.target.value);
+  };
 
   useEffect(() => {
     dispatch(fetchSubreddits());
   }, [dispatch]);
 
+  console.log(subreddits);
   return (
-    <StyledContainer>
+    <StyledDropdown onChange={handleChange}>
       {subreddits.map((subreddit) => {
-        return (
-          <StyledItem
-            key={subreddit.id}
-            className={`${selectedSubreddit === subreddit.url && `active`}`}>
-            <button
-              onClick={() => dispatch(setSelectedSubreddit(subreddit.url))}>
-              <img
-                src={
-                  subreddit.icon_img ||
-                  `https://api.adorable.io/avatars/25/${subreddit.display_name}`
-                }
-                alt={subreddit.display_name}
-                style={{ border: `3px solid ${subreddit.primary_color}` }}
-                className='icon'
-              />
-              <span>{subreddit.display_name}</span>
-            </button>
-          </StyledItem>
-        );
+        return <option key={subreddit.id}>{subreddit.display_name}</option>;
       })}
-    </StyledContainer>
+    </StyledDropdown>
   );
 };
-export default Subreddit;
+export default DropDown;
